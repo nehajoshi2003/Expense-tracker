3d91dd61d5f11d7c8d119949f9d1255871927b55
Neha Joshi <nehajoshi@Nehas-Laptop.local>
2025-04-27 12:25:20 +0530

    Update project files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/Expense-tracker/", // Change this to match your GitHub repo name
-});
+    base: "/Expense-tracker/",
+    plugins: [react()],
+  });
+  
